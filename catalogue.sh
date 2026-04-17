33a655847881e0acf5b7b04ff169a099012c26b9
malli arunkumar <malliarunkumar777@gmail.com>
2026-04-17 11:51:07 +0530

    changed something

---
+++ catalogue.sh
@@ -0,0 +1,28 @@
+!#/bin/bash
+
+source ./common.sh
+
+APP_NAME=catalogue
+
+chech_root
+
+APP_SETUP
+
+NODEJS_SETUP
+
+SYSTEMD_SETUP
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "$(date "+%Y-%m-%d :%H:%M:%S") |Products already loaded ... $Y SKIPPING $N"
+fi
+
+APP_RESTART
+
--- common.sh
+++ common.sh
@@ -9,6 +9,8 @@ Y="\e[33m"
 N="\e[0m"
 
 mkdir -p $LOGS_FOLDER
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws88s.online
 
 START_TIME=$(date +%s)
 echo "$(date "+%Y-%m-%d :%H:%M:%S") |scrit started executing at :$(date)" | tee -a $LOGS_FILE
@@ -28,6 +30,59 @@ VALIDATE(){
     else
         echo -e "$(date "+%Y-%m-%d :%H:%M:%S") |$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
+}
+
+NODEJS_SETUP(){
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Disabling NodeJS Default version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "Enabling NodeJS 20"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Install NodeJS"
+
+    npm install  &>>$LOGS_FILE
+    VALIDATE $? "Installing dependencies"
+
+}
+APP_SETUP(){
+    id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+        VALIDATE $? "Creating system user"
+    else
+        echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+    fi
+
+    mkdir -p /app 
+    VALIDATE $? "Creating app directory"
+
+    curl -o /tmp/$APPNAME.zip https://roboshop-artifacts.s3.amazonaws.com/$APPNAME-v3.zip  &>>$LOGS_FILE
+    VALIDATE $? "Downloading $APPNAME code"
+
+    cd /app
+    VALIDATE $? "Moving to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+
+    unzip /tmp/$APPNAME.zip &>>$LOGS_FILE
+    VALIDATE $? "Uzip $APPNAME code"
+}
+SYSTEMD_SETUP(){
+    cp $SCRIPT_DIR/$APPNAME.service /etc/systemd/system/$APPNAME.service
+    VALIDATE $? "Created systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $APPNAME  &>>$LOGS_FILE
+    systemctl start $APPNAME
+    VALIDATE $? "Starting and enabling $APPNAME"
+}    
+
+APP_RESTART(){          
+    systemctl restart catalogue
+    VALIDATE $? "Restarting catalogue"
 }
 
 PRINT_TOTAL_TIME(){
